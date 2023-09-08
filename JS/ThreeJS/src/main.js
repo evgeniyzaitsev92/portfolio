b6bf9c30fafba60b942e5d6097c957cbc1048cbc
ezaitsev <ezaitsev>
2023-09-08 15:18:24 +0300

    Commit9

--- JS/ThreeJS/src/main.js
+++ JS/ThreeJS/src/main.js
@@ -41,9 +41,12 @@ scene.add(light);
 let props = {
     R: 1,
     N: 6,
-    H: 2,
+    H: 2
+};
+let pointsData = [];
+let surf = {
     hasFaces: true,
-    hasWireFrame: false,
+    hasWireFrame: false
 };
 
 const propsFolder = gui.addFolder('Properties');
@@ -59,28 +62,69 @@ propsFolder.add(props, 'H', 0, 16, 0.25).name('Height').onChange(function(val){
     props.H = val;
     sendToServer(props);
 });
-propsFolder.add(props, 'hasFaces').name('Faces').onChange(function(val){
-    props.hasFaces = val;
-    sendToServer(props);
+propsFolder.add(surf, 'hasFaces').name('Faces').onChange(function(val){
+    surf.hasFaces = val;
+    process(pointsData);
+
 });
-propsFolder.add(props, 'hasWireFrame').name('Wireframe').onChange(function(val){
-    props.hasWireFrame = val;
-    sendToServer(props);
+propsFolder.add(surf, 'hasWireFrame').name('Wireframe').onChange(function(val){
+    surf.hasWireFrame = val;
+    process(pointsData);
 });
 propsFolder.open();
 
-// Triangle
+// Build cone 
+function createTriangles(pointsData, surf){
+    let triangles = [];
+    for(let i = 0; i < pointsData.length; i++){
+        const triangle = pointsData[i];
+        let points = [];
+        points.push(new THREE.Vector3(triangle[0][1], triangle[0][2], triangle[0][3]));
+        points.push(new THREE.Vector3(triangle[1][1], triangle[1][2], triangle[1][3]));
+        points.push(new THREE.Vector3(triangle[2][1], triangle[2][2], triangle[2][3]));
+        let geometry = new THREE.BufferGeometry().setFromPoints(points);
+    
+        if(surf.hasFaces == true){
+            const triangleMaterial = new THREE.MeshPhongMaterial({
+                color: 0xfc0303,
+                side: THREE.DoubleSide, 
+                flatShading: true
+            });
+            var triangleMesh = new THREE.Mesh(geometry, triangleMaterial);
+            triangleMesh.castShadow = true; //default is false
+            triangleMesh.receiveShadow = false;
+            triangles.push(triangleMesh);
+        }
+
+        if(surf.hasWireFrame == true){
+            var line = new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: 0x09ff00 }));
+            triangles.push(line);
+        }
+    }
+    return triangles;
+};
+
+let trianglesOld = [];
 let triangles = [];
-function update(elements){
-    triangles.forEach(element => {
+function removeOld(){
+    trianglesOld.forEach(element => {
         scene.remove(element); 
     });
+};
 
-    elements.forEach(element => {
+function buildNew(){
+    triangles.forEach(element => {
         scene.add(element);
         triangles.push(element);
     });
-}
+};
+
+function process(pointsData){
+    removeOld();
+    triangles = createTriangles(pointsData, surf);
+    trianglesOld = triangles;
+    buildNew();
+};
 
 // Plane
 const planeGeometry = new THREE.PlaneGeometry(20, 20);
@@ -90,6 +134,35 @@ plane.position.y = -1.75;
 plane.receiveShadow = true;
 scene.add(plane);
 
+// Send info to server
+function sendToServer(val){
+    const postData = JSON.stringify({props: val});
+
+    const postOptions = {
+        host: 'localhost',
+        port: '8000',
+        path: '/',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(postData)
+        }
+    };
+
+    const postReq = http.request(postOptions, function(res){
+        let body = [];
+        res.on('data', (chunk) => {
+            body.push(chunk);
+        }).on('end', () => {
+            body = Buffer.concat(body).toString();
+            pointsData = JSON.parse(body);
+            process(pointsData);
+        });
+    });
+    postReq.end(postData);
+};
+sendToServer(props);
+
 // Cone manupulation
 let mouseDown = false, mouseX = 0, mouseY = 0;
 
@@ -97,9 +170,7 @@ function onMouseMove(event){
     if(!mouseDown){
         return;
     }
-
     event.preventDefault();
-
     let deltaX = event.clientX - mouseX;
     let deltaY = event.clientY - mouseY;
     mouseX = event.clientX;
@@ -139,30 +210,6 @@ function rotateScene(deltaX, deltaY){
 }
 addMouseHandler(view);
 
-// Send info to server
-function sendToServer(val){
-    const postData = JSON.stringify({props: val});
-
-    const postOptions = {
-        host: 'localhost',
-        port: '8000',
-        path: '/',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(postData)
-        }
-    };
-
-    const postReq = http.request(postOptions, function(res){
-        res.setEncoding('utf8');
-        res.on('data', function(chunk){});
-    });
-  
-    postReq.write(postData);
-    postReq.end();
-};
-
 // Resize window
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize(){
@@ -178,7 +225,3 @@ function render(){
     renderer.setClearColor(0x000000, 0.0);
 }
 render();
-
-export default{
-    update, props
-}
